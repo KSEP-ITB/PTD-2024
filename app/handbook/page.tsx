013a3275db5b4c919111ae9f79779ff51c68dd1f
unknown <naufarrelzhafifabhista@gmail.com>
2024-10-10 15:01:48 +0700

    feat: added cards and search bar

--- app/handbook/page.tsx
+++ app/handbook/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import SearchBar from '@/components/HBSearchBar'
+import Card from '@/components/HBCard'
 import Jumbotron from '@/components/HBJumbotron'
 import React from 'react'
 
@@ -5,6 +9,17 @@ const Handbook = () => {
   return (
     <>
       <Jumbotron/>
+      <section className = "bg-[#0F389B] px-[6vw] py-[2vw]">
+        <SearchBar/>
+        <Card
+          HBDay = "Day 1"
+          HBTitle = "Personal Finance"
+        />
+        <Card
+          HBDay = "Day 2"
+          HBTitle = "Investment World"
+        />
+      </section>
     </>
   )
 }
