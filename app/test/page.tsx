54d4a9eba550cdae3979639a9b96d99323a16ce3
AtqiyaHaydar <ashford1524@gmail.com>
2024-10-12 19:18:35 +0700

    feat: init uploadthing

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import ImageUpload from '@/components/image-upload';
+import { UploadButton } from '@/utils/uploadthing';
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ImageUpload />
+    </div>
+  )
+}
+
+export default page
--- auth.ts
+++ auth.ts
@@ -1,29 +1,29 @@
-// import NextAuth from "next-auth"
-// import Credentials from "next-auth/providers/credentials"
-// // import { saltAndHashPassword } from "@/utils/password"
-// import { signInSchema } from "./lib/schemas"
-// import { getUserByUsername } from "./actions/user-actions"
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+// import { saltAndHashPassword } from "@/utils/password"
+import { signInSchema } from "./lib/schemas"
+import { getUserByUsername } from "./actions/user-actions"
  
-// export const { handlers, signIn, signOut, auth } = NextAuth({
-//   providers: [
-//     Credentials({
-//       credentials: {
-//         email: { label: "Email", type: "text" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       authorize: async (credentials) => {
-//         let user = null
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        let user = null
 
-//         const { username, password } = await signInSchema.parseAsync(credentials)
+        const { username, password } = await signInSchema.parseAsync(credentials)
  
-//         user = await getUserByUsername(username, password)
+        user = await getUserByUsername(username, password)
  
-//         if (!user) {
-//           throw new Error("User not found.")
-//         }
+        if (!user) {
+          throw new Error("User not found.")
+        }
  
-//         return user
-//       },
-//     }),
-//   ],
-// })
+        return user
+      },
+    }),
+  ],
+})
