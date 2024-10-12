f63b7b8bf7e402bfcb8d609ff66a6c6a37b5e13a
AtqiyaHaydar <ashford1524@gmail.com>
2024-10-12 13:45:22 +0700

    style: resposnive navbar and footer and header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,18 +32,18 @@ const socialMediaItems = [
 const Footer = () => {
   return (
     <div className='wrapper flex flex-col items-center gap-y-12 z-20'>
-      <div className='flex items-center gap-x-4'>
+      <div className='flex flex-col md:flex-row items-center gap-4'>
         <Image 
           src={KSEPLogo}
           alt='Logo KSEP'
           width={48}
           height={48}
         />
-        <h5 className='text-orange-gradient font-bold text-2xl'>Kelompok Studi Ekonomi dan Pasar Modal</h5>
+        <h5 className='text-orange-gradient font-bold text-center text-[18px] md:text-2xl'>Kelompok Studi Ekonomi dan Pasar Modal</h5>
       </div>
       <div className='flex flex-col gap-y-4 items-center'>
         <p className='text-center text-orange-gradient'>Our Social Media</p>
-        <div className='flex gap-x-20'> 
+        <div className='flex gap-x-10 lg:gap-x-20'> 
           {socialMediaItems.map((item, index) => {
             const Icon = item.logo
             return (
@@ -54,7 +54,7 @@ const Footer = () => {
           })}
         </div>
       </div>
-      <p className='font-bold text-[#ED5126] text-center text-xl'>
+      <p className='font-bold text-[#ED5126] text-center text-[16px] md:text-xl'>
         © 2024 Kelompok Studi Ekonomi dan Pasar Modal ITB. All Rights Reserved
       </p>
     </div>
